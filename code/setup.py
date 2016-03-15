3e38273185ba37c77f8e9559175ea69889b9a454
jdmille2 <john.david.miller@intel.com>
2016-03-15 15:14:52 -0700

    added setup.py to install think*.  import via 'import thinkbayes'

---
+++ code/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ThinkBayes',
+      version='1.0',
+      description='ThinkBayes book code',
+      author='Allen Downey',
+      author_email='allen.downey@olin.edu',
+      url='https://github.com/AllenDowney',
+      py_modules=['thinkbayes', 'thinkstats', 'thinkplot'],
+     )
