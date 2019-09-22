e55794df225b0f2980489cb0c887f0f3171f3d5f
davidaking <19488561+davidaking@users.noreply.github.com>
2019-09-22 21:29:04 +0100

    Update columns.py

--- code/columns.py
+++ code/columns.py
@@ -3,6 +3,9 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2012 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+Modified on Sunday 22nd September 2019 as a test
+
 """
 
 import csv
